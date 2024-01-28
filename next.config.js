e28423d360a381370b638550f9baaa1154dab58c
taichi-kanaya <tk.attakait@gmail.com>
2024-01-28 14:10:06 +0900

    トップページへアクセスされたときは記事一覧を表示するリダイレクト設定方法を変更

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        // トップページは記事一覧ページにリダイレクト
+        source: "/",
+        destination: "/stocks",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
--- src/app/page.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-
-// トップページは一覧ページにする
-const page = () => {
-  redirect(`/stocks`);
-};
-
-export default page;
